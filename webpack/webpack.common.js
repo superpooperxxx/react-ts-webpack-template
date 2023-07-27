c19c8526a57672937eeca777739010dbfbd5311f
Kyrylo Haiduk <superpooper@Kyrylos-MacBook-Pro-2.local>
2023-07-27 22:37:33 +0300

    splitted webpack configs

---
+++ webpack/webpack.common.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "src", "index"),
+  output: {
+    path: path.resolve(__dirname, "..", "build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(sc|sa)ss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "resolve-url-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: "asset/inline",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "public", "index.html"),
+    }),
+  ],
+};
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,60 +1,9 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common.js");
 
-module.exports = () => {
-  return {
-    mode: "development",
-    entry: path.resolve(__dirname, "..", "src", "index.tsx"),
-    output: {
-      path: path.resolve(__dirname, "..", "build"),
-      filename: "bundle.js",
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".jsx", ".js"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(ts|js)x?$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "babel-loader",
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.(sc|sa)ss$/,
-          use: [
-            "style-loader",
-            "css-loader",
-            "resolve-url-loader",
-            {
-              loader: "sass-loader",
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-          type: "asset/resource",
-        },
-        {
-          test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
-          type: "asset/inline",
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "..", "public", "index.html"),
-      }),
-    ],
-  };
+module.exports = ({ mode }) => {
+  const modeConfig = require(`./webpack.${mode}.js`);
+  const config = merge(commonConfig, modeConfig);
+
+  return config;
 };
